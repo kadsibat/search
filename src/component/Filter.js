81400fcaa9a5650ccb6c6aefed06aafbab05111f
sibel <sibelkadirkyhn@gmail.com>
2022-05-25 18:32:05 +0300

    dd

--- src/component/Filter.js
+++ src/component/Filter.js
@@ -6,7 +6,7 @@ const Filter = () => {
   const [arama, setArama] = useState("");
   const [genderstate, setGenderState] = useState("");
   const [lang, setLang] = useState([]);
-  const [ortak, setOrtak] = useState([])
+  const [ortak, setOrtak] = useState([]);
   const [allLangs, setAllLangs] = useState([
     "CHINESE",
     "GERMAN",
@@ -17,11 +17,16 @@ const Filter = () => {
     "RUSSIAN",
     "ARABIC",
     "TURKEY",
-  ])
- console.log(lang);
-useEffect(() => {
- console.log(genderstate);
-}, [genderstate])
+  ]);
+  console.log(lang);
+  useEffect(() => {
+    console.log(genderstate);
+    console.log("ortakk", ortak);
+  }, [genderstate]);
+  useEffect(() => {
+    console.log("merhaba", lang);
+    console.log("ortakk", ortak);
+  }, [lang]);
 
   const handleChange = (e) => {
     e.preventDefault();
@@ -29,44 +34,46 @@ useEffect(() => {
   };
 
   const genderChange = (gender) => {
-    setGenderState(gender);
-    setOrtak([...ortak,gender])
-
+    if (!genderstate.includes(gender)) {
+      setGenderState(gender);
+      setOrtak([...ortak, gender]);
+    }
   };
 
   const langChange = (e) => {
-    setLang([...lang,e]);
-    setOrtak([...ortak,e])
+    console.log("langg", lang);
+    if (!lang.includes(e)) {
+      setLang([...lang, e]);
+      setOrtak([...ortak, e]);
+    }
+
+    // console.log(e);
   };
 
   return (
     <div className="filter">
-      {console.log(allLangs)}
-      {console.log(ortak)}
+      {/* {console.log(allLangs)}
+      {console.log(ortak)} */}
       <div className="row">
         <div className="col-sm-4 col-6">
-          {/* <select name="categoria" onChange={langChange}>
-            <option value="none" >
-             languages
-            </option>
-            <option value="englısh">english</option>
-            <option value="german">german</option>
-            <option value="turkey">turkey</option>
-          </select> */}
           <div
             className="button d-flex justify-content-center align-items-center dropdown-toggle border"
             id="dp1"
             data-bs-toggle="dropdown"
             aria-expanded="false"
-  
           >
             Languages
           </div>
           <ul className="dropdown-menu dd-language" aria-labelledby="dp1">
-         {allLangs?.map((lang,key)=>
-           <li className="dropdown-item" key={key} onClick={()=>langChange({lang})}>{lang}</li>
-        )}
-    
+            {allLangs?.map((lang, key) => (
+              <li
+                className="dropdown-item"
+                key={key}
+                onClick={() => langChange(lang)}
+              >
+                {lang}
+              </li>
+            ))}
           </ul>
         </div>
         <div className="col-sm-4 col-6">
@@ -83,18 +90,28 @@ useEffect(() => {
             id="dp2"
             data-bs-toggle="dropdown"
             aria-expanded="false"
-            
-            
           >
             Gender
           </div>
           <ul className="dropdown-menu" aria-labelledby="dp2">
             <li>
-              <a className="dropdown-item" href="# " onClick={()=>genderChange("male")}>Male</a>
+              <a
+                className="dropdown-item"
+                href="# "
+                onClick={() => genderChange("male")}
+              >
+                Male
+              </a>
             </li>
 
             <li>
-              <a className="dropdown-item" href="# " onClick={()=>genderChange("famele")}>Female</a>
+              <a
+                className="dropdown-item"
+                href="# "
+                onClick={() => genderChange("famele")}
+              >
+                Female
+              </a>
             </li>
           </ul>
         </div>
